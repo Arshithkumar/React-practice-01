cae31b7b7682d16589e71f2bdf4eee269b9d534c
Arshithkumar <arrarama2k@gmail.com>
2024-06-25 00:20:46 +0530

    add load animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import './Home.css';
+
 export default function Home() {
     let [users, setUsers] = useState([])
-    let [search, setSearch] = useState([])
+    let [search, setSearch] = useState('')
     let [country, sCountry] = useState('')
+    let [loading, setLoading] = useState(true)
 
     const api_avatar = () => {
         fetch('https://jsonplaceholder.typicode.com/users').then(res => {
@@ -19,21 +21,25 @@ export default function Home() {
     }
 
     let filteredUser = users.filter((user) => {
-        return user.name.includes(search)
+        return user.name.toLowerCase().includes(search.toLowerCase())
     })
 
     let finalData = filteredUser.filter((val) => {
-        return val.company.name.includes(country)
+        return val.company.name.toLowerCase().includes(country.toLowerCase())
     })
 
 
 
     useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
         api_avatar();
     }, [])
 
 
     return (
+
         <div className='text-center my-2'>
             <TextField id="outlined-basic" label="search User" varient="outlined" onChange={(e) => setSearch(e.target.value)} />
             <TextField
@@ -41,7 +47,7 @@ export default function Home() {
                 select
                 label="Select"
                 defaultValue=""
-                helperText="Please select your company"
+                helperText="select your company"
                 onChange={(e) => sCountry(e.target.value)}
             >
                 {users.map((option) => (
@@ -51,27 +57,43 @@ export default function Home() {
                 ))}
             </TextField>
 
-            <div className='position-relative py-2 px-4'>
-                {
-                    finalData.map((user) => {
-                        return (
-                            <Link to={`/user/${user.id}`} key={user.id} state={{ data: user }}>
-                                <User
-                                    name={user.name}
-                                    id={user.id}
-                                    // address={user.address}
-                                    company={user.company}
-                                    website={user.website}
-                                    phone={user.phone}
-                                    email={user.email}
-                                    username={user.username}
-                                />
-                            </Link>
-                        )
-                    })
-                }
-            </div>
+            {
+                loading ? (
+                    <div className="sk-chase">
+                        <div className="sk-chase-dot"></div>
+                        <div className="sk-chase-dot"></div>
+                        <div className="sk-chase-dot"></div>
+                        <div className="sk-chase-dot"></div>
+                        <div className="sk-chase-dot"></div>
+                        <div className="sk-chase-dot"></div>
+                    </div>
+                ) : (
+
+
+                    <div className='position-relative py-2 px-4'>
+                        {
+                            finalData.map((user) => {
+                                return (
+                                    <Link to={`/user/${user.id}`} key={user.id} state={{ data: user }}>
+                                        <User
+                                            name={user.name}
+                                            id={user.id}
+                                            // address={user.address}
+                                            company={user.company}
+                                            website={user.website}
+                                            phone={user.phone}
+                                            email={user.email}
+                                            username={user.username}
+                                        />
+                                    </Link>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
         </div>
+
     )
 
 }
